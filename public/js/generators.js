adde0c3f51b803622a325f2e77481c122cc35008
Eric Whitfield <ewhitfield@xlanes.lcl>
2020-04-23 17:00:46 -0400

    basic skeleton

---
+++ public/js/generators.js
@@ -0,0 +1,15 @@
+let gen = document.getElementsByClassName("generator");
+
+let dungoal = 
+
+let title = document.createElement("h2");
+
+
+function addTable(){
+
+    
+    array.forEach(element => {
+    
+    });
+}
+
